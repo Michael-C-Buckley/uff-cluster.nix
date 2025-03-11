82c9af949ce7c2a545038f2178f9f0fc472eeb5a
Michael Buckley <michaelcbuckley@proton.me>
2025-03-11 19:17:50 -0400

    Added my home configs via hjem

--- base.nix
+++ base.nix
@@ -1,6 +1,7 @@
 # Base configuration shared among all devices
 {inputs, ...}: {
   imports = with inputs; [
+    inputs.michael-home.nixosModules.hjem.default
     ./modules
     ./network
     ./pkgs
