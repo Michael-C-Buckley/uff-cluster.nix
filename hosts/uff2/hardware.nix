bbf55a84c5ca85a705f0d8cd4bd0f647468431b2
Michael Buckley <michaelcbuckley@proton.me>
2025-03-11 22:08:35 -0400

    Updated UFF2's boot UUID

--- hosts/uff2/hardware.nix
+++ hosts/uff2/hardware.nix
@@ -1,7 +1,7 @@
 {...}: {
   fileSystems = {
     "/boot" = {
-      device = "/dev/disk/by-uuid/8E01-5577";
+      device = "/dev/disk/by-uuid/F50D-E713";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
