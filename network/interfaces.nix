5a94c2e0bf51cd2f65d66ed2fbd23f3a954c550e
Michael Buckley <michaelcbuckley@proton.me>
2025-03-11 22:29:16 -0400

    Updated network interfaces

--- network/interfaces.nix
+++ network/interfaces.nix
@@ -8,32 +8,28 @@
     prefixLength = prefix;
   };
 in {
-  networking = {
-    networkmanager.unmanaged = ["wlp2s0"];
-    wireless = {
-      enable = true;
-      # Network info here
-    };
-    # Add the first interface into a bridge and a default route
-    bridges.br0.interfaces = ["eno1"];
+  # WIP: Wifi connection after adding age-encryption
+  # environment.etc."NetworkManager/system-connections/wifi.nmconnection".source = "";
 
+  networking = {
+    # Add the first interface into a bridge and disable DHCP
+    bridges.br0.interfaces = ["eno1"];
     interfaces = {
+      # Bridge-related configs
+      eno1.useDHCP = false;
+      br0.ipv4.addresses = [(addr brIP 24)];
+
       # Wifi gets DHCP
       wlp2s0.useDHCP = true;
-      # Disable DHCP for the onboard ethernet
-      eno1.useDHCP = false;
+
       # L3 reachable loopback addresses for host and anycast
       lo.ipv4.addresses = [
         (addr loopback 32)
         (addr "192.168.61.0" 32)
       ];
+
       # This is the 2.5G USB NIC
-      enusb1.ipv4.addresses = [
-        (addr enusb1.ipv4.addr enusb1.ipv4.prefixLength)
-      ];
-      br0.ipv4.addresses = [
-        (addr brIP 24)
-      ];
+      enusb1.ipv4.addresses = [(addr enusb1.ipv4.addr enusb1.ipv4.prefixLength)];
     };
   };
 }
